275059a9148c8dcdbd150edf66287d65b5e9f617
Bimbel Rumba <yrki@vmi3126847.contaboserver.net>
2026-04-09 16:43:36 +0700

    Create DocType Rumba Tahun Ajaran

---
+++ rumbanext/bimbel_rumba/doctype/rumba_tahun_ajaran/test_rumba_tahun_ajaran.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Yayasan Rumba Kita Indonesia and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestRUMBATahunAjaran(FrappeTestCase):
+	pass
